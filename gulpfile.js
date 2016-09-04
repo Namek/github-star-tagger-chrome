839b5e7d469a90d7128a7e12d34d798b128700ea
Kamil Dąbrowski <nameczanin@gmail.com>
2016-09-04 17:45:27 +0200

    build(gulpfile): run tasks sequentially, watch assets

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@
 const gulp = require('gulp')
 const ts = require('gulp-typescript')
 const clean = require('gulp-clean')
+const sequence = require('gulp-sequence')
 
 const Path = {
   tsConfig: 'tsconfig.json',
-  notCode: './src/**/!(*.ts)',
+  copyable: './src/**/*.@(html|css|json|png|js)',
   decls: './src/decls',
   outDir: './build'
 }
@@ -14,24 +15,28 @@ const Path = {
 
 let tsProject = ts.createProject(Path.tsConfig)
 
-gulp.task('compile', () => {
-  return tsProject.src()
+gulp.task('compile', () =>
+  tsProject.src()
     .pipe(ts(tsProject))
     .js.pipe(gulp.dest(Path.outDir))
-})
+)
 
-gulp.task('copy_assets', () => {
-    gulp.src(Path.notCode)
-      .pipe(gulp.dest(Path.outDir))
-})
+gulp.task('copy_assets', () =>
+  gulp.src(Path.copyable)
+    .pipe(gulp.dest(Path.outDir))
+)
 
 gulp.task('watch', () => {
-  const paths = [...require(`./${Path.tsConfig}`).files, Path.tsConfig]
-  let watcher = gulp.watch(paths, ['compile'])
-  watcher.on('change', (event) => {
-    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    if (event.path)
+  const tsPaths = [...require(`./${Path.tsConfig}`).files, Path.tsConfig]
+  let tsWatcher = gulp.watch(tsPaths, ['compile'])
+  tsWatcher.on('change', evt => {
+    console.log('File ' + evt.path + ' was ' + evt.type + ', compiling...')
     tsProject = ts.createProject(Path.tsConfig)
+  })
+
+  let assetWatcher = gulp.watch(Path.copyable, ['copy_assets'])
+  assetWatcher.on('change', evt => {
+    console.log('File ' + evt.path + ' was ' + evt.type + ', copying assets...')
   })
 })
 
@@ -40,6 +45,6 @@ gulp.task('clean', () =>
     .pipe(clean())
 )
 
-gulp.task('build', ['clean', 'copy_assets', 'compile'])
+gulp.task('build', sequence('clean', 'copy_assets', 'compile'))
 
-gulp.task('default', ['build', 'watch'])
+gulp.task('default', sequence('build', 'watch'))
